7bb0cd039f3db696290108612ea61263ec2f5302
mitchel <edahmitchel@example.com>
2022-08-05 10:59:32 +0100

    added listner

--- reservation .js
+++ reservation .js
@@ -1,15 +1,5 @@
-const entryDate = document.querySelector('#inputEntrydate').value;
-console.log(entryDate)
-const exitDate = document.querySelector('#inputExitdate').value;
-console.log(exitDate)
-const space = document.querySelector('#inputSpace').value;
-const carModel = document.querySelector('#carModel').value;
-const licensePlate = document.querySelector('#licensePlate').value;
-const firstName = document.querySelector('#inputFirstName').value;
-const lastName = document.querySelector('#inputLastName').value;
-const email = document.querySelector('#inputEmail').value;
-const password = document.querySelector('#inputPassword').value;
-const phonenumber = document.querySelector('#inputPhoneNumber').value;
+const submitBtn = document.querySelector('#submit-btn');
+
 // alert("hello")
 
 // if(firstName)console.log(firstName)
@@ -26,11 +16,26 @@ const phonenumber = document.querySelector('#inputPhoneNumber').value;
 //     password : password,
 //     pNumber : phonenumber
 // }
-let peoplesData = JSON.stringify({entDate: entryDate,exDate: exitDate,space : space, model : carModel,lPlate : licensePlate,fName : firstName,lName : lastName,email : email,password : password,pNumber : phonenumber})
 
+submitBtn.addEventListener('click', (e)=>{
+    const entryDate = document.querySelector('#inputEntrydate').value;
+    console.log(entryDate)
+    const exitDate = document.querySelector('#inputExitdate').value;
+    console.log(exitDate)
+    const space = document.querySelector('#inputSpace').value;
+    const carModel = document.querySelector('#carModel').value;
+    const licensePlate = document.querySelector('#licensePlate').value;
+    const firstName = document.querySelector('#inputFirstName').value;
+    const lastName = document.querySelector('#inputLastName').value;
+    const email = document.querySelector('#inputEmail').value;
+    const password = document.querySelector('#inputPassword').value;
+    const phonenumber = document.querySelector('#inputPhoneNumber').value;
+    let peoplesData = JSON.stringify({entDate: entryDate,exDate: exitDate,space : space, model : carModel,lPlate : licensePlate,fName : firstName,lName : lastName,email : email,password : password,pNumber : phonenumber})
+    localStorage.setItem("theData", peoplesData);
+    console.log(JSON.parse(localStorage.getItem("theData"))); 
+})
 
-
-localStorage.setItem("theData", peoplesData);
 let info = localStorage.getItem("theData");
-console.log(JSON.parse(localStorage.getItem("theData", peoplesData))); 
+
+
 
--- submit.js
+++ submit.js
@@ -4,9 +4,13 @@ console.log(localStorage);
 
 let fullName = document.querySelector("#fullName");
 let plate = document.querySelector("#plate");
-let randId = document.querySelector("#randId");
+let randId = Math.floor(Math.random() * 1000000);
+let space =document.querySelector("#space")
 // fullName.innerHTML = "Ediomo Peters";
 
 console.log(localStorage.entDate);
 
-fullName.textContent = localStorage.fName;
+fullName.textContent = localStorage.fName + " " + localStorage.lName;
+plate.textContent = localStorage.lPlate;
+space.textContent = localStorage.space;
+randId.textContent = randId;
